3c654297a561001788a29317427ab5d7108ab669
Bilalsajai1 <contact.bilal02@gmail.com>
2024-02-19 18:52:32 +0100

    V1

---
+++ src/main/java/dao/DaoImpl.java
@@ -0,0 +1,17 @@
+package dao;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+@Repository("dao")
+public class DaoImpl implements IDao{
+    @Override
+    public double getData() {
+        /*
+        se connecter a la BD pour récuperer la température
+         */
+        System.out.println("Version base de données");
+        double temp = Math.random()*40;
+        return temp;
+    }
+}
---
+++ src/main/java/pres/Pres_Par_instanciation_Statique.java
@@ -0,0 +1,19 @@
+package pres;
+
+import dao.DaoImpl;
+import metier.MetierImpl;
+
+
+
+public class Pres_Par_instanciation_Statique {
+    public static void main(String[] args) {
+       /*
+       ces trois lignes de codes font  l'injection des dépendances
+  Par instanciation statique => new
+        */
+        DaoImpl dao = new DaoImpl();
+        MetierImpl metier = new MetierImpl();
+        metier.setDao(dao);
+        System.out.println("Résultats :" + metier.calcul());
+    }
+}
